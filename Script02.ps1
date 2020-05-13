1e21b8bc9f8d278922a93a0acb3d989a631b2c19
Administrateur@ACME.LOCAL <Administrateur@ACME.LOCAL>
2020-05-13 19:56:11 +0200

    Ajout du script02

---
+++ Script02.ps1
@@ -0,0 +1,12 @@
+﻿#Script02
+#Récupére la liste des membres d'un groupe AD
+
+$nameofgroup = Read-Host "Merci de Rentrer le Nom du groupe AD"
+
+#Demande le nom du groupe pour lequel on veux la liste, résultat dans un fichier créer automatiquement
+
+Get-ADGroupMember -Identity $nameofgroup | Select-Object -Property name | out-file "c:\Scripts\Resultats_$nameofgroup.txt"
+
+#Texte de réponse à la demande
+
+Write-Host "Le fichier Resultats_$nameofgroup.txt à bien été créer"
